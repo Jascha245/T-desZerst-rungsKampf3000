2e09d357eeb4a5f941e3c3c153fe65960738630b
Jascha245 <yppiehageltoster525@gmail.com>
2022-10-03 11:30:46 +0200

    Initial commit

---
+++ Room.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TödesZerstörungsKampf3000
+{
+    internal class Room
+    {
+        enum RoomChar
+        {
+            Wall = 'W',
+            Floor = ' ',
+            Enemy = 'E',
+            Player = 'P',
+            Pillar = 'O',
+            Door = 'D',
+            Bag = 'B',
+            Boss = 'K'
+        }
+
+        public char[][] room;
+        public int width;
+        public int height;
+
+        public Room()
+        {
+            room[height][width] = BuildRoom();
+        }
+        private char[][] BuildRoom()
+        {
+
+            return;
+        }
+    }
+}
